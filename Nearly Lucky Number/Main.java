e0164553b28979792620e414958f5c7a39cfe2a9
Swastik2740S <onamverma8@gmail.com>
2025-12-30 15:02:12 +0530

    lucky numbers

---
+++ Nearly Lucky Number/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int count = 0;
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '4' || s.charAt(i) == '7'){
+                count++;
+            }
+        }
+
+        
+        if(count == 7 || count == 4){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+
+
+
+    }
+}
